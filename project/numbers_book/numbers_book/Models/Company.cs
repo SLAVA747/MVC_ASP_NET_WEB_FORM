cc7bd4d0c7a13897b049bb8e8cba18808e03a9f8
SLAVA747 <43703034+SLAVA747@users.noreply.github.com>
2018-12-11 10:52:56 +0700

    Scaffold-DbContext
    Цепляем BD через Scaffold-DbContext
    "Host=localhost;Port=49708;Database=TB_DB;Username=postgres;Password=1234567890" Npgsql.EntityFrameworkCore.PostgreSQL -o Models

---
+++ project/numbers_book/numbers_book/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace numbers_book.Models
+{
+    public partial class Company
+    {
+        public Company()
+        {
+            Numbers = new HashSet<Numbers>();
+        }
+
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanySite { get; set; }
+        public string CompanyTelephone { get; set; }
+
+        public virtual ICollection<Numbers> Numbers { get; set; }
+    }
+}
